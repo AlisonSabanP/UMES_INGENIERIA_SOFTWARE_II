bc1505f0579c17fbd230f7916a564de19434434a
AlisonSabanP <alisonsabanpiche@gmail.com>
2025-05-13 16:46:44 -0600

    test

--- test_juego.py
+++ test_juego.py
@@ -4,39 +4,39 @@ import unittest
 from juego import comparar_jugada
 
 class TestCompararJugada(unittest.TestCase):
-    def test_piedra_vs_piedra(self):
+    def test_piedrapiedra(self):
         result = comparar_jugada("piedra", "piedra")
         self.assertEqual(result, 0)
     
-    def test_piedra_vs_papel(self):
+    def test_piedrapapel(self):
         result = comparar_jugada("piedra", "papel")
         self.assertEqual(result, -1)
     
-    def test_piedra_vs_tijera(self):
+    def test_piedratijera(self):
         result = comparar_jugada("piedra", "tijera")
         self.assertEqual(result, 1)
     
-    def test_papel_vs_piedra(self):
+    def test_papelpiedra(self):
         result = comparar_jugada("papel", "piedra")
         self.assertEqual(result, 1)
     
-    def test_papel_vs_papel(self):
+    def test_papelpapel(self):
         result = comparar_jugada("papel", "papel")
         self.assertEqual(result, 0)
     
-    def test_papel_vs_tijera(self):
+    def test_papeltijera(self):
         result = comparar_jugada("papel", "tijera")
         self.assertEqual(result, -1)
     
-    def test_tijera_vs_piedra(self):
+    def test_tijerapiedra(self):
         result = comparar_jugada("tijera", "piedra")
         self.assertEqual(result, -1)
     
-    def test_tijera_vs_papel(self):
+    def test_tijerapapel(self):
         result = comparar_jugada("tijera", "papel")
         self.assertEqual(result, 1)
     
-    def test_tijera_vs_tijera(self):
+    def test_tijeratijera(self):
         result = comparar_jugada("tijera", "tijera")
         self.assertEqual(result, 0)
 
